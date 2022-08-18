d16bfd98c9eaee00bf148eb9922048d8c62f2a00
rahimedu <edwin.duche82@gmail.com>
2022-08-18 16:54:06 -0400

    commit tree 873998c1ea5f9d6121204440d09c2c2620ffbcab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    p "Hello, programmer!"
+end
+
+def greet name
+p "Hello, #{name}!"
+end
+
+def greet_with_default name= 'programmer'
+p "Hello, #{name}!"
+end
+
+def add num1, num2
+num1 + num2
+end
+
+def halve num1
+if num1.class != Integer
+    return nil
+end
+num1 / 2
+end
+
